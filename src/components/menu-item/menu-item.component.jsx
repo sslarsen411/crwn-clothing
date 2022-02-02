cec87e95fca0a6dfe747a546b4d96e426bcc463f
Scott Larsen <sslarsen411@gmail.com>
2022-02-01 17:29:52 -0800

    Adding routing to menu component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom' // v6
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, id, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='content'>
-      <div className='title'>{title}</div>
-      <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, id, size, linkUrl }) => {
+  const navigate = useNavigate() // v6
+
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='content'>
+        <div className='title'>{title}</div>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default MenuItem
