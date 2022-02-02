bbc28c765e7597187ac0f73f8cf4d8f0ceb4df9e
Scott Larsen <sslarsen411@gmail.com>
2022-02-01 16:45:38 -0800

    Routing to work with react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import {
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 
-function App () {
+const HatsPage = () => (
+  <div>
+    <h1>HATS HATS HATS!</h1>
+  </div>
+)
+
+function NoMatch () {
   return (
     <div>
-      <HomePage />
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to='/'>Go to the home page</Link>
+      </p>
     </div>
+  )
+}
+
+function App () {
+  return (
+  // Different than video 80
+    <Routes>
+      <Route exact path='/' element={<HomePage />} />
+      <Route path='/hats' element={<HatsPage />} />
+      <Route path='*' element={<NoMatch />} />
+    </Routes>
   )
 }
 
