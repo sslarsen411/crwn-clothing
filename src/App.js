5c1a18d86ab94731d9030186a74012be68bb30f8
Scott Larsen <sslarsen411@gmail.com>
2022-02-09 10:22:56 -0800

    added checkout page, checkout item component and updated cart to use selector

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import {
 } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-
-import './App.css'
+import { auth, createUserProfileDocument} from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 import Header from './components/header/header.component'
 import Footer from './components/footer/footer.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import CheckoutPage from './pages/checkout/checkout.component'
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component'
-import { auth, createUserProfileDocument} from './firebase/firebase.utils'
-import { setCurrentUser } from './redux/user/user.actions'
-import { selectCurrentUser } from './redux/user/user.selectors'
-//import getYear from './functions/functions';'./functions/functions'
+
+import './App.css'
 
 function NoMatch () {
   return (
@@ -33,7 +33,6 @@ function NoMatch () {
 
 const currentDate = new Date()
 const currYr =  currentDate.getFullYear()
-
 class App extends React.Component {
   unsubscribeFromAuth = null
 
@@ -68,6 +67,7 @@ class App extends React.Component {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
+          <Route path='/checkout' element={<CheckoutPage />} />
           {/* v6 Redirect */}
           <Route 
           path="/signin" element={this.props.currentUser ? 
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+import { useNavigate } from 'react-router-dom'
+
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({ cartItems }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
-      {cartItems.length
-        ? (
-            cartItems.map(cartItem => (
-              <CartItem key={cartItem.id} item={cartItem} />
-            ))
-          )
-        : (
-          <span className='empty-message'>Your cart is empty</span>
-          )}
+const CartDropdown = ({ cartItems, dispatch }) => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    dispatch(toggleCartHidden())
+    navigate('/checkout')
+  }
+  return (
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.length
+          ? (
+              cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+              ))
+            )
+          : (
+            <span className='empty-message'>Your cart is empty</span>
+            )}
+      </div>
+      <CustomButton onClick={handleClick}>go to checkout</CustomButton>
     </div>
-    <CustomButton>go to checkout</CustomButton>
-  </div>
-)
+  )
+}
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
