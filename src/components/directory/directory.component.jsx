a6427cd67c194238010b9f03200d7afb80a1ea79
Scott Larsen <sslarsen411@gmail.com>
2022-02-10 06:24:21 -0800

    Modified shop page to use redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,26 +1,21 @@
 import React from 'react'
 import MenuItem from '../menu-item/menu-item.component'
-import DIRECTORY_DATA from '../../data/directory.data'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors'
+
 import './directory.styles.scss'
 
-class Directory extends React.Component {
-  constructor () {
-    super()
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+)
 
-    this.state = {
-      sections: DIRECTORY_DATA// END sections
-    }
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
-  render () {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
-
-export default Directory
+export default connect(mapStateToProps)(Directory)
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,14 @@
+import DIRECTORY_DATA from '../../data/directory.data'
+
+const INITIAL_STATE = {
+  sections: DIRECTORY_DATA
+}
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default directoryReducer
