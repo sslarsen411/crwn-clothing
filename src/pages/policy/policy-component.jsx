788277bccc1d0e1cb8ac7d3e6012bb0b122697ff
Scott Larsen <sslarsen411@gmail.com>
2022-02-15 06:42:01 -0800

    Refactored policy code

--- src/pages/policy/policy-component.jsx
+++ src/pages/policy/policy-component.jsx
@@ -8,19 +8,21 @@ import './policy-component.styles.scss'
 const PolicyPage = () => {
   const [policy, setPolicy] = React.useState(null)
   const params = useParams()
+  const baseUrl = 'https://www.mojoimpact.net/php_scripts/ajax_getContent.php'
 
-  const fetchData = () => {
-    axios.get('https://www.mojoimpact.net/php_scripts/ajax_getContent.php?site=Crown%20Clothing&inPolicy=' + params.policyId)
-      .then(response => {
-        setPolicy(response.data)
-        document.getElementById('policy').innerHTML = policy
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  fetchData()
+  axios.get(baseUrl, {
+    params: {
+      inPolicy: params.policyId,
+      site: 'Crown Clothing LTD'
+    }
+  })
+    .then(response => {
+      setPolicy(response.data)
+      document.getElementById('policy').innerHTML = policy
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 
   return (
     <div className='policy-page'>
